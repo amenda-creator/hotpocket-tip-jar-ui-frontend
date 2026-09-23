38bb838c41ea0cd470beccf226bbf501b0aafca7
amenda-creator <testamenda@gmail.com>
2026-09-23 14:35:31 +0530

    Update store.ts

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../features/auth/authSlice';
 import snackbarReducer from '../features/snackbar/snackbarSlice';
-
+//ppp
 export const store = configureStore({
   reducer: {
     auth: authReducer,
